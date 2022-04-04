8a8546006267e85704434db2d50e068af7ab70a2
Doug Moreira <dougmoreira@Dougs-MBP-219.lan>
2022-04-04 17:59:43 -0400

    Loader and Greeting components

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react'
 
 const NavBar = () => {
   const [toggleMenu ,setToggleMenu] = useState(false)
+  const options = ["Market", "Exchange", "Tutorials", "Wallets"]
 
   const NavBarItem = ({title, classProps}) => {
     return (
@@ -19,7 +20,7 @@ const NavBar = () => {
         <img src={logo} alt="logo" className='w-32 cursor-pointer'></img>
       </div>
       <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
-          {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=>(
+          {options.map((item, index)=>(
             <NavBarItem key={item + index} title={item}/>
           ))}
           <li className='bg-[#2952E3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]'>
@@ -36,7 +37,7 @@ const NavBar = () => {
             {toggleMenu && (
         <ul className="z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
             <li className="text-xl w-full my-2"> <AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market", "Exchange", "Tutorials", "Wallets"].map(
+            {options.map(
                 (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
         </ul>
